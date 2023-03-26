04f9c79c195f6c9b51bb86b0d832e2eeb2951958
Ariel Aguilar <ariel.aguilarit@gmail.com>
2023-03-26 17:42:54 -0300

    fix/pre-commit

--- src/__test__/App.spec.js
+++ src/__test__/App.spec.js
@@ -1,15 +1,15 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
+
 describe('<App />', () => {
-  beforeEach(() => {
-    render(<App />);
-  });
   test('Render Component', () => {
+    render(<App />);
     const text = 'Welcome to React Router from my pc!!!!';
     const title = screen.queryByText(text);
     expect(title).toBeInTheDocument();
   });
   test('Sing in onclick button', () => {
+    render(<App />);
     const textBtn = 'Sign In';
     const btn = screen.getByText(textBtn);
     expect(btn).toBeInTheDocument();
